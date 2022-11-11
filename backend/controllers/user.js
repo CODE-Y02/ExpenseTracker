907c1a73415ca24a758dfe58e7ebbd2cc49b345f
yatharth <lakhatey@gmail.com>
2022-11-11 17:14:57 +0530

    Added password encryption using bcrypt

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,16 +1,19 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 exports.postSignUp = async (req, res, next) => {
   try {
-    let newUser = {
-      name: req.body.userName,
-      email: req.body.email,
-      password: req.body.password,
-    };
+    let name = req.body.userName;
+    let email = req.body.email;
+    let password = req.body.password;
 
-    let user = await User.create(newUser);
-    console.log(user);
-    res.json({ success: true, message: "SignUp successful" });
+    const saltRounds = 10;
+    bcrypt.hash(password, saltRounds, async function (err, hash) {
+      console.log(err);
+      // Store hash in your password DB.
+      await User.create({ name, email, password: hash });
+      res.json({ success: true, message: "SignUp successful" });
+    });
   } catch (error) {
     console.log("\n \n \n \n ");
     console.log(error);
@@ -44,20 +47,25 @@ exports.postSignIn = async (req, res, next) => {
       return res.status(404).json({ success: false, error: "User Not Found" });
     }
 
-    if (!(user[0].password === password)) {
-      //wrong password
-      return res
-        .status(401)
-        .json({
+    const hash = user[0].password;
+
+    bcrypt.compare(password, hash, function (err, result) {
+      // result == true
+
+      if (err) throw new Error("Something Went Wrong !!!");
+      if (result) {
+        res.json({ success: true, message: "Login successfull" });
+      } else {
+        return res.status(401).json({
           success: false,
           error: "Wrong password",
           message: "User not authorized",
         });
-    }
+      }
+    });
 
-    res.json({ success: true, message: "Login successfull" });
     // console.log("\n \n \n");
-    // console.log(user[0].password == password);
+    // console.log(isPassWordValid);
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
     // console.log("\n \n \n");
